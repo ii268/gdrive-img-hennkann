5f2ac25ceceba11c1e0c444d03b923d789f76881
ibuibu268 <92432744+ii268@users.noreply.github.com>
2026-01-02 21:12:54 +0900

    googledrive仕様変更に対応

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ const gdive2 = gdive1.value
 const ken = gdive2.substr(8 ,16);
 if (ken == "drive.google.com") {
 const code = gdive2.substr(32 ,33);
-const imgurl = "https://drive.google.com/uc?id="+code;
+const imgurl = "https://lh3.googleusercontent.com/d/"+code;
 document.getElementById("kennse").style.display ="block";
 document.getElementById( "end" ).value = imgurl ;
 } else {  
@@ -15,4 +15,5 @@ function copy(){
 var copyTarget = document.getElementById("end");
 copyTarget.select();
 document.execCommand("Copy");
+
 }
